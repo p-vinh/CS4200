f8a27f13469729b1748fdf15ea4d33f93a2c42ef
random-debug <85326677+p-vinh@users.noreply.github.com>
2024-02-07 20:02:59 -0800

    todo

--- 8-Queens/main.py
+++ 8-Queens/main.py
@@ -8,15 +8,36 @@ the same row, column or
 diagonal
 """
 
-class EightQueens:
+class Board:
     def __init__(self):
         self.board = [[0 for _ in range(8)] for _ in range(8)]
         self.queens = 0
 
     def is_safe(self, row, col):
-        pass
+        # check if a queen can be placed at row, col
+        # return True if it's safe, False otherwise
 
+        # 3 Possible cases:
+        # 1. Queen is in the midde of the board
+        # 2. Queen is in the edge of the board
+        # 3. Queen is in the corner of the board
+
+        # check surrounding cells of the piece
+
+        # check vertical
+
+        # check horizontal
+
+        # check diagonal
+
+        
+        return True
     def place_queen(self, col):
+
+        # base case
+        if col >= 8:
+            return True
+
         pass
 
     def solve(self):
@@ -24,26 +45,16 @@ class EightQueens:
 
     def print_solution(self):
         pass
-    
-class Queen:
-    def __init__(self, row, col):
-        self.row = row
-        self.col = col
 
-    def potential_move(self):
-        # return a list of potential moves
-        
-        # vertical
-        for i in range(8):
-            if i != self.row:
-                yield (i, self.col)
-        # horizontal
-        for i in range(8):
-            if i != self.col:
-                yield (self.row, i)
-        pass
-    
     def __str__(self):
-        return f"({self.row}, {self.col})"
-    
+        return "\n".join(" ".join(str(cell) for cell in row) for row in self.board)
+
+def main():
+    board = Board()
+    board.solve()
+    board.print_solution()
+
+if __name__ == "__main__":
+    main()
+
     
