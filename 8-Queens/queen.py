5773a84170d770631a8d82a6f276cc22ba154a7a
random-debug <85326677+p-vinh@users.noreply.github.com>
2024-02-08 18:12:21 -0800

    Queens problem finished

--- 8-Queens/queen.py
+++ 8-Queens/queen.py
@@ -7,13 +7,14 @@ attack each other by being in
 the same row, column or
 diagonal
 """
+import unittest
+import pygame
 
 class Eight_Queens:
     def __init__(self):
         self.board = [[0 for _ in range(8)] for _ in range(8)]
         self.queens = 0
 
-    # Checks if a queen can be placed at the current position
     def is_safe(self, row, col):
         # 3 Possible cases:
         # 1. Queen is in the midde of the board
@@ -29,20 +30,34 @@ class Eight_Queens:
                     return False
         
         # check if there is a queen in the same column
-        col = sum([self.board[i][col] for i in range(8)])
-        if (col > 1):
-            return False
+        for i in range(8):
+            if (self.board[i][col] == 1):
+                return False
 
         # check if there is a queen in the same row
-        row = sum(self.board[row])
-        if (row > 1):
-            return False
-
-        # check if there is a queen in the same diagonal
         for i in range(8):
-            for j in range(8):
-                if (i + j == row + col) or (i - j == row - col) and self.board[i][j] == 1:
-                    return False
+            if (self.board[row][i] == 1):
+                return False
+
+        # upper diagonal left
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if self.board[i][j] == 1:
+                return False
+        # upper diagonal right
+        for i, j in zip(range(row, -1, -1), range(col, 8, 1)):
+            if self.board[i][j] == 1:
+                return False
+
+        # lower diagonal right
+        for i, j in zip(range(row, 8, 1), range(col, 8, 1)):
+            if self.board[i][j] == 1:
+                return False
+        
+        # lower diagonal left
+        for i, j in zip(range(row, 8, 1), range(col, -1, -1)):
+            if self.board[i][j] == 1:
+                return False
+
         return True
 
     # debug function
@@ -52,11 +67,9 @@ class Eight_Queens:
 
 
     def solve(self, col):
-        self.board.__str__()
-
         if self.queens == 8:
             print("8 Queens have been placed")
-            return True
+            return self.get_all_positions()
 
         for row in range(8):
             if self.is_safe(row, col):
@@ -71,13 +84,44 @@ class Eight_Queens:
 
         return False
 
+    def get_all_positions(self):
+        return [(i, j) for i in range(8) for j in range(8) if self.board[i][j] == 1]
+
     def __str__(self):
         return "\n".join(" ".join(str(cell) for cell in row) for row in self.board)
 
 
-if __name__ == "__main__":
-    board = Eight_Queens()
-    # Test cases for is_safe
+SQUARE_SIZE = 50
+GRID_SIZE = 8
+QUEEN = pygame.transform.scale(pygame.image.load('8-Queens\images\chess-queen.svg'), (SQUARE_SIZE, SQUARE_SIZE))
+queen_width, queen_height = QUEEN.get_size()
 
+WHITE = (255, 255, 255)
+BROWN = (139, 69, 19)
 
-    
+pygame.init()
+screen = pygame.display.set_mode((GRID_SIZE * SQUARE_SIZE, GRID_SIZE * SQUARE_SIZE))
+
+for x in range(GRID_SIZE):
+    for y in range(GRID_SIZE):
+        rect = pygame.Rect(x*SQUARE_SIZE, y*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+        pygame.draw.rect(screen, WHITE if (x+y) % 2 == 0 else BROWN, rect)
+
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        solver = Eight_Queens()
+        solver.solve(0)
+    if pygame.key.get_pressed()[pygame.K_SPACE]:
+         
+        for x, y in solver.get_all_positions():
+            screen.blit(QUEEN, (x*SQUARE_SIZE + 6, y*SQUARE_SIZE + 5))
+
+        pygame.display.flip()
+
+pygame.quit()
+
