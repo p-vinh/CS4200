cf6b9bafd4d7f4ac71de0cdf8fcd375b2f91f37b
Vinh Pham <85326677+p-vinh@users.noreply.github.com>
2024-03-06 10:30:34 -0800

    Update model.py

--- ChessML/test/model.py
+++ ChessML/test/model.py
@@ -1,109 +1,142 @@
-import tensorflow as tf
-import pandas as pd
+from peewee import *
+import base64
+
+db = SqliteDatabase("2021-07-31-lichess-evaluations-37MM.db")
+
+
+class Evaluations(Model):
+    id = IntegerField()
+    fen = TextField()
+    binary = BlobField()
+    eval = FloatField()
+
+    class Meta:
+        database = db
+
+    def binary_base64(self):
+        return base64.b64encode(self.binary)
+
+
+db.connect()
+LABEL_COUNT = 37164639
+print(LABEL_COUNT)
+eval = Evaluations.get(Evaluations.id == 1)
+print(eval.binary_base64())
+
+import os
+import torch
 import numpy as np
-import chess
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader, IterableDataset, random_split
+import pytorch_lightning as pl
+from random import randrange
+
+
+class EvaluationDataset(IterableDataset):
+    def __init__(self, count):
+        self.count = count
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        idx = randrange(self.count)
+        return self[idx]
+
+    def __len__(self):
+        return self.count
+
+    def __getitem__(self, idx):
+        eval = Evaluations.get(Evaluations.id == idx + 1)
+        bin = np.frombuffer(eval.binary, dtype=np.uint8)
+        bin = np.unpackbits(bin, axis=0).astype(np.single)
+        eval.eval = max(eval.eval, -15)
+        eval.eval = min(eval.eval, 15)
+        ev = np.array([eval.eval]).astype(np.single)
+        return {"binary": bin, "eval": ev}
+
+
+dataset = EvaluationDataset(count=LABEL_COUNT)
+
+
+import time
 from collections import OrderedDict
-from operator import itemgetter
-import chess.pgn
-import chess.engine
-import piece_eval
-
-# import keras.models as load_model
-path_to_model = "./ChessML/latest-model"
-
-global model
-model = tf.saved_model.load(path_to_model)
 
 
-def get_possible_moves_data(current_board):
-    data = []
-    moves = list(current_board.legal_moves)
-    for move in moves:
-        from_square, to_square = get_move_features(move)
-        row = np.concatenate(
-            (get_board_features(current_board), from_square, to_square)
+class EvaluationModel(pl.LightningModule):
+    def __init__(self, learning_rate=1e-3, batch_size=1024, layer_count=10):
+        super().__init__()
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        layers = []
+        for i in range(layer_count - 1):
+            layers.append((f"linear-{i}", nn.Linear(808, 808)))
+            layers.append((f"relu-{i}", nn.ReLU()))
+        layers.append((f"linear-{layer_count-1}", nn.Linear(808, 1)))
+        self.seq = nn.Sequential(OrderedDict(layers))
+
+    def forward(self, x):
+        return self.seq(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch["binary"], batch["eval"]
+        y_hat = self(x)
+        loss = F.l1_loss(y_hat, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+    def train_dataloader(self):
+        dataset = EvaluationDataset(count=LABEL_COUNT)
+        return DataLoader(
+            dataset, batch_size=self.batch_size, num_workers=2, pin_memory=True
         )
-        data.append(row)
-
-    board_feature_names = chess.SQUARE_NAMES
-    move_from_feature_names = ["from_" + square for square in chess.SQUARE_NAMES]
-    move_to_feature_names = ["to_" + square for square in chess.SQUARE_NAMES]
-
-    columns = board_feature_names + move_from_feature_names + move_to_feature_names
-
-    df = pd.DataFrame(data=data, columns=columns)
-
-    for column in move_from_feature_names:
-        df[column] = df[column].astype(float)
-    for column in move_to_feature_names:
-        df[column] = df[column].astype(float)
-    return df
 
 
-def predict(df_eval, imported_model):
-    col_names = df_eval.columns
-    dtypes = df_eval.dtypes
-    predictions = []
-    for row in df_eval.iterrows():
-        example = tf.train.Example()
-        for i in range(len(col_names)):
-            dtype = dtypes[i]
-            col_name = col_names[i]
-            value = row[1][col_name]
-            if dtype == "object":
-                value = bytes(value, "utf-8")
-                example.features.feature[col_name].bytes_list.value.extend([value])
-            elif dtype == "float64":
-                example.features.feature[col_name].float_list.value.extend([value])
-            elif dtype == "int":
-                example.features.feature[col_name].int64_list.value.extend([value])
-        predictions.append(
-            imported_model.signatures["predict"](
-                examples=tf.constant([example.SerializeToString()])
-            )
-        )
-    return predictions
+configs = [
+    {"layer_count": 4, "batch_size": 512},
+    #  {"layer_count": 6, "batch_size": 1024},
+]
+for config in configs:
+    version_name = f'{int(time.time())}-batch_size-{config["batch_size"]}-layer_count-{config["layer_count"]}'
+    logger = pl.loggers.TensorBoardLogger(
+        "lightning_logs", name="chessml", version=version_name
+    )
+    trainer = pl.Trainer(precision=16, max_epochs=1, logger=logger)
+    model = EvaluationModel(
+        layer_count=config["layer_count"],
+        batch_size=config["batch_size"],
+        learning_rate=1e-3,
+    )
+    # trainer.tune(model)
+    # lr_finder = trainer.tuner.lr_find(model, min_lr=1e-6, max_lr=1e-3, num_training=25)
+    # fig = lr_finder.plot(suggest=True)
+    # fig.show()
+    trainer.fit(model)
+    break
 
 
-def get_board_features(board):
-    board_features = []
-    for square in chess.SQUARES:
-        board_features.append(str(board.piece_at(square)))
-    return board_features
+def svg_url(fen):
+    fen_board = fen.split()[0]
+    return SVG_BASE_URL + fen_board
 
 
-def get_move_features(move):
-    from_ = np.zeros(64)
-    to_ = np.zeros(64)
-    from_[move.from_square] = 1
-    to_[move.to_square] = 1
-    return from_, to_
+def show_index(idx):
+    eval = Evaluations.select().where(Evaluations.id == idx + 1).get()
+    batch = dataset[idx]
+    x, y = torch.tensor(batch["binary"]), torch.tensor(batch["eval"])
+    y_hat = model(x)
+    loss = F.l1_loss(y_hat, y)
+    print(
+        f"Idx {idx} Eval {y.data[0]:.2f} Prediction {y_hat.data[0]:.2f} Loss {loss:.2f}"
+    )
+    print(f"FEN {eval.fen}")
+    display(SVG(url=svg_url(eval.fen)))
 
 
-
-
-def find_best_moves(current_board, model, proportion=0.5):
-    moves = list(current_board.legal_moves)
-    df_eval = get_possible_moves_data(current_board)
-    predictions = predict(df_eval, model)
-    good_move_probas = []
-
-    for prediction in predictions:
-        proto_tensor = tf.make_tensor_proto(prediction["probabilities"])
-        proba = tf.make_ndarray(proto_tensor)[0][1]
-        good_move_probas.append(proba)
-
-    dict_ = dict(zip(moves, good_move_probas))
-    dict_ = OrderedDict(sorted(dict_.items(), key=itemgetter(1), reverse=True))
-
-    best_moves = list(dict_.keys())
-
-    return best_moves[0 : int(len(best_moves) * proportion)]
-
-def evaluate_board(board):
-    """Return the evaluation of a board"""
-    evaluation = 0
-    for square in chess.SQUARES:
-        piece = str(board.piece_at(square))
-        evaluation = evaluation + piece_eval.get_piece_value(piece, square)
-    return evaluation
+for i in range(5):
+    idx = randrange(LABEL_COUNT)
+    show_index(idx)
